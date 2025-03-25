2d8b44668a54a3968c7c420c5384ce9142e1f386
Gyehwan Baek <b99hwan@gmail.com>
2025-03-25 21:36:36 +0900

    프로그래머스 - 짝지어 제거하기(해결)

--- programmers/짝지어_제거하기.java
+++ programmers/짝지어_제거하기.java
@@ -4,33 +4,17 @@ package programmers;
 import java.util.*;
 class 짝지어_제거하기 {
     public int solution(String s) {
-        // 1. s.length만큼의 반복문을 순회
-        boolean flag = true;
-        StringBuilder sb = new StringBuilder(s);
-        while(flag) {
-            flag = false;
-            Stack<Character> stack = new Stack<>();
-            for (int i = 0; i < sb.length(); i++) {
-                // 2. stack을 초기화하고 push, 다음 반복문에서 pop. 지금 문자열과 비교
-                char nowChar = sb.charAt(i);
-                if (stack.isEmpty()) { // 비어있으면 그냥 넣고 끝
-                    stack.push(nowChar);
-                    continue;
-                }
-                char target = stack.pop();
-                // 2-1. 같으면 subString으로 자른 후에 새로운 문자열로 반복문 재시작
-                if (target == nowChar) {
-                    sb.delete(i-1, i+1);
-                    flag = true;
-                    continue;
-                } else { // 2-2. 다르면 stack.push 후 반복문 돌기
-                    stack.push(nowChar);
-                }
+        Stack<Character> stack = new Stack<>();
+        // 1. s의 문자를 하나씩 읽으면서 stack에 push
+        for (int i = 0; i < s.length(); i++) {
+            // 스택이 비어있지 않고 stack.peek() == c 이면 stack.pop
+            if(!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+                stack.pop();
+            } else {
+                // else이면 push
+                stack.push(s.charAt(i));
             }
         }
-        if (sb.length() == 0) {
-            return 1;
-        }
-        return 0;
+        return stack.isEmpty() ? 1 : 0;
     }
 }
