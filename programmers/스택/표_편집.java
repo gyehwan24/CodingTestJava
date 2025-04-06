99b602c6506ca667d9bc217e009e41ba65b2a90f
Gyehwan Baek <b99hwan@gmail.com>
2025-04-06 23:56:07 +0900

    프로그래머스 - 표 편집 아이디어

---
+++ programmers/스택/표_편집.java
@@ -0,0 +1,17 @@
+package programmers.스택;
+// https://school.programmers.co.kr/learn/courses/30/lessons/81303
+
+import java.io.*;
+import java.util.*;
+class 표_편집 {
+    public String solution(int n, int k, String[] cmd) {
+        String answer = "";
+        // 삭제한 행들을 Stack에 저장해놓는다.
+        // C -> Stack.push()
+        // Z -> Stack.pop()
+        // 기존 표에는 삭제한 행을 어떻게 표시?
+        // 기존 표를 구성하는 표는 어떤 자료구조? ArrayList? LinkedList?
+        //
+        return answer;
+    }
+}
