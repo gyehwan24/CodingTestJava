2da0491b18171acf7da6175c9f88918e2b3124e4
Gyehwan Baek <b99hwan@gmail.com>
2025-04-20 13:10:18 +0900

    프로그래머스 - 할인행사 (리팩토링)

--- programmers/해시/할인행사.java
+++ programmers/해시/할인행사.java
@@ -5,25 +5,17 @@ import java.util.*;
 class 할인행사 {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        // discount 배열의 원소를 10개씩 (슬라이딩윈도우) 해시맵으로 만든다.
-        HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
+        HashMap<String, Integer> wantMap = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
         }
-        for (int start = 0; start <= discount.length - 10; start++) {
-            if (isSatisfy(map, want, number)) answer++;
-            // 반복문이 1회 끝나면 discount 배열의 슬라이딩 윈도우를 오른쪽으로 한 칸 움직인다.
-            if (start+10 >= discount.length) break;
-            map.put(discount[start], map.get(discount[start]) - 1);
-            map.put(discount[start+10], map.getOrDefault(discount[start+10], 0) + 1);
+        for (int i = 0; i <= discount.length - 10; i++) {
+            HashMap<String, Integer> discountMap = new HashMap<>();
+            for (int j = i; j < i + 10; j++) {
+                discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
+            }
+            if (discountMap.equals(wantMap)) answer++;
         }
         return answer;
-    }
-    public boolean isSatisfy(HashMap<String, Integer> map, String[] want, int[] number) {
-        for (int i = 0; i < want.length; i++) {
-            if (!map.containsKey(want[i])) return false;
-            if (map.get(want[i]) != number[i]) return false;
-        }
-        return true;
     }
 }
